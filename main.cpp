8f6509648dea2475f415a1e37cab9e75db0abfde
Dalton Senseman <drsenseman4328@eagle.fgcu.edu>
2020-11-05 17:35:38 -0500

    Setup commit of main.cpp file

--- main.cpp
+++ main.cpp
@@ -1,3 +1,14 @@
+/**
+ * @file main.ccp
+ * CRN 3031 demo
+ *      @brief
+ *
+ *      Code for c++ demo of a double linked list for use in educational purposes in understanding how to better use
+ *      Git and GitHub, how a merge is done, branching, and conflict resolution.
+ *
+ *      @author Dalton Senseman
+ *      @version .01 11/5/2020
+ */
 #include <iostream>
 
 int main() {
